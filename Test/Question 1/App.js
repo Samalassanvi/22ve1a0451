4b6b3bfcd0d172670909e9a80f97fd61fe14278f
Samala Sri Ssanvi <137263620+Samalassanvi@users.noreply.github.com>
2025-07-18 12:22:22 +0530

    Add files via upload

---
+++ Test/Question 1/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          URL
+          <div className="URL Shortner page"> 
+            <header className="original long URL "></header>
+          </div>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
